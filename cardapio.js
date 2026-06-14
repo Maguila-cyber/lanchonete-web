ce3c54f97581ee2d2f401e4582151dc9f64d44fd
Maguila-cyber <jhomnatha.silva@academico.unirv.edu.br>
2026-06-14 22:49:33 +0000

    feat: inicia logica do cardapio

---
+++ cardapio.js
@@ -0,0 +1,7 @@
+const cardapio = [
+  { nome: "Coxinha", preco: 5.50 },
+  { nome: "Pastel", preco: 6.00 },
+  { nome: "Suco Natural", preco: 4.50 }
+];
+
+console.log("Cardápio carregado:", cardapio);
